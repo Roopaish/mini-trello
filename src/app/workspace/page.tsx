976e027229debc1fc4d710c53555a32443fdcf85
roopaish <rupesh39943@gmail.com>
2024-02-03 00:31:30 +0545

    no redirect if not authenticated

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,7 +1,6 @@
 import { KanbanBoard } from "@/components/board";
 import NewTaskDialog from "@/components/board/new-task-dialog";
 import { isUserAuthenticated } from "@/lib/session";
-import { redirect } from "next/navigation";
 
 const getInitialTasks = async () => {
   try {
@@ -27,7 +26,7 @@ export default async function page() {
   const initialTasks = await getInitialTasks();
 
   if (!isAuthenticated) {
-    redirect("/");
+    // redirect("/");
   }
 
   return (
